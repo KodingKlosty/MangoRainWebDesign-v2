192ed3a8672bce9c860a4941306c3a8708eccc1e
James Klostermeyer Jr <42751399+KodingKlosty@users.noreply.github.com>
2020-05-02 10:15:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/api/form', form)
 // Serve Static assets if in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static(path.join('client/build')));
 
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
